c662c21b5dbea9f22110ae83b85157c0291dbfb5
Luke Eyles <modularluke@gmail.com>
2022-03-13 16:50:02 +1100

    images

--- images/thumb.py
+++ images/thumb.py
@@ -1,16 +1,17 @@
 from PIL import Image
 import os
 
-path = "images/blender/"
-fns = os.listdir(path)
+paths = ["images/blender/","images/minecraft/","images/origami/"]
 
-for fn in fns:
-    try:
-        print(path+fn)
-        image = Image.open(path+fn)
-    except:
-        print("not an image")
-        break
+for path in paths:
+    fns = os.listdir(path)
+    for fn in fns:
+        try:
+            print(path+fn)
+            image = Image.open(path+fn)
+        except:
+            print("not an image")
+            break
 
-    image.thumbnail((400, 400))
-    image.save(path+"thumb/"+fn)
+        image.thumbnail((600, 600))
+        image.save(path+"thumb/"+fn)
